190c88d22f9e7a6bd6617cfd2b6c7d48743bb425
Vismaya S <86291573+VismayaSathyan@users.noreply.github.com>
2022-06-13 14:42:59 +0530

    Add files via upload

---
+++ countdown-timer/script.js
@@ -0,0 +1,48 @@
+const countcontainer = document.querySelector('.countdown-container')
+const days1 = document.querySelector('#days');
+const hours1 = document.querySelector('#hours');
+const mins1= document.querySelector('#mins');
+const secs1 = document.querySelector('#seconds')
+const newYears =new Date('06/19/2022');
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24; // milliseconds 
+//const today = new Date();
+//console.log(today);
+let timerId;
+
+
+function countDown(){
+    const today = new Date();
+    const timeSpan = newYears - today;
+    
+    
+    console.log(timeSpan);
+    
+    if(timeSpan <= -day){
+        countcontainer.innerText = "HOPE YOU HAD A GOOD BIRTHDAY";
+        clearInterval(timerId);
+        return
+    }
+
+    if(timeSpan<=0){
+        countcontainer.innerHTML = "HAPPY BIRTHDAYYYY!!";
+        clearInterval(timerId);
+        return;
+    }
+
+    const days = Math.floor(timeSpan/day);
+    const hours = Math.floor((timeSpan % day)/ hour);
+    const minutes = Math.floor((timeSpan% hour)/ minute);
+    const seconds = Math.floor((timeSpan % minute) / second);
+
+    days1.innerHTML = days;
+    hours1.innerHTML = hours;
+    mins1.innerHTML = minutes;
+    secs1.innerHTML = seconds;
+
+
+}
+//console.log(newYears)
+timerId = setInterval(countDown, second);
